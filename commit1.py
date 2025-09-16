e38cdb7c786cdc40d1c3da51cd9c84c169898e7e
JANG JI EUN <gsilver312@jnu.ac.kr>
2025-09-16 09:50:48 +0900

    Create commit1.py

---
+++ commit1.py
@@ -0,0 +1,12 @@
+korean = 90
+math = 80
+english = 100
+
+total = korean + math + english
+average = total // 3
+
+print("국어", korean)
+print("수학", math)
+print("영어", english)
+print("합계", total)
+print("평균", average)
